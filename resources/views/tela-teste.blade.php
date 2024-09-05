829238deabe2be0004f6d408b14bc86b39cfe2cd
otavio1d <otaviordias77@gmail.com>
2024-09-05 10:39:11 -0300

    criacao do projeto, tailwind, e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+ <title> tela de teste </title>
+</head>
+<body>
+  <h1 class="text-red-500 bg-black p-4 rounded lg">
+    Hello world!
+  </h1>
+</body>
+</html>
